731ce6d671888f133c1a8212e951ce25e22c9417
bglossner <benglossner@gmail.com>
2021-10-13 00:57:50 -0700

    fixed asset url base

--- webapp/src/constants.ts
+++ webapp/src/constants.ts
@@ -2,7 +2,7 @@ import process from "process";
 
 export const IS_DEV: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
-export const ASSETS_URL = "https://raw.githubusercontent.com/CSC480-Group1/csc466_project/main";
+export const ASSETS_URL = "https://raw.githubusercontent.com/CSC480-Group1/CSC-480/main";
 
 export const MARKDOWN_ASSETS_URL = `${ASSETS_URL}/code/markdown`;
 export const NOTEBOOK_ASSETS_URL = `${ASSETS_URL}/code/notebooks`;
