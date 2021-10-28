d72f676a63e1df7195cfb39874139005a9becbaf
Jordan Powers <jordanpowers1227@gmail.com>
2021-10-28 13:18:03 -0700

    AI Tester

---
+++ code/games/ai_battle.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+import random
+import minimax
+import eval_funcs
+import mcts
+from Games import *
+from BoardTest import getBoardValue
+
+
+class Player(ABC):
+    @abstractmethod
+    def play(self, game: CGame):
+        pass
+
+class MonteCarloPlayer(Player):
+    def play(self, game: CGame):
+        move = mcts.mcts(game, game.getPlayer())
+        print("Mcts plays {}".format(move))
+        game.doMove(move)
+
+class MinimaxPlayer(Player):
+    def __init__(self, eval_func):
+        self._eval_func = eval_func
+        minimax.set_depth_limit(3)
+
+    def play(self, game: CGame):
+        move = minimax.minimax_best_move(game, self._eval_func)
+        print("Minimax plays {}".format(move))
+        game.doMove(move)
+
+class RandomPlayer(Player):
+    def play(self, game: CGame):
+        moves = game.getValidMoves()
+        assert len(moves) > 0
+        move = random.choice(moves)
+        print("Random plays {}".format(move))
+        game.doMove(move)
+
+game = OthelloGame()
+player1 = MonteCarloPlayer()
+player2 = MinimaxPlayer(eval_funcs.eval_othello_1)
+# player2 = RandomPlayer()
+
+while True:
+    print(game.showBoard())
+    if len(game.getValidMoves()) == 0:
+        break
+    player1.play(game)
+
+    print(game.showBoard())
+    if len(game.getValidMoves()) == 0:
+        break
+    player2.play(game)
+
+val = getBoardValue()
+
+if val > 0:
+    print("Black wins!")
+elif val < 0:
+    print("White wins!")
+else:
+    print("Draw!")
+
